05ec81bfd3265bafd9cdbca9ef45a6db61e5a71a
1x31744 <koopahello@gmail.com>
2026-06-06 23:29:55 +0100

    trying to go into protected, currently tripple fault?

--- src/kernel/basic_kernel.asm
+++ src/kernel/basic_kernel.asm
@@ -1,3 +1,4 @@
+; no org code starts at 0x0900 though
 start:
     mov ax, cs
     mov ds, ax
@@ -5,13 +6,13 @@ start:
     mov si, hello_string
     call print_string
 
-    jmp $
+    jmp enter_protected
 
 print_string:
     mov ah, 0Eh
 
 print_char:
-    lodsb
+    lodsb ; sets al = [DS:SI++]
 
     cmp al, 0
     je done
@@ -23,4 +24,55 @@ print_char:
 done:
     ret
 
-hello_string db 'Hello World!, i am lytlnyblOS', 0
+enter_protected:
+    cli ;disable interrupts
+    lgdt [gdtr] ; load GDT registor with start address of GDT
+    mov eax, cr0
+    or eax, 1 ;set protection enable bit in control register 0 (cr0)
+    mov cr0, eax
+
+    ; perform far jump to selector 08h (offset into GDT, pointing at a 32bit
+    ; PM code segment descriptor)
+    ; to load CS with proper PM32 descriptor)
+
+    jmp dword 08h:p_mode_main
+    jmp enter_protected
+
+[bits 32]
+p_mode_main:
+    mov ax, 10h
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+    ;mov dword [0xB8000], 0x07410741
+hang:
+    jmp hang
+
+hello_string db 'Hello World!, i am lytlnyblOS, running in real mode', 0
+
+gdt:
+
+gdt_null:
+    dq 0
+gdt_code:
+    dw 0FFFFh
+    dw 0
+    db 0
+    db 10011010b ;
+    db 11001111b
+    db 0
+gdt_data:
+    dw 0FFFFh
+    dw 0
+    db 0
+    db 10010010b
+    db 11001111b
+    db 0
+gdt_end:
+gdtr:
+    dw gdt_end - gdt
+    dd gdt
+
