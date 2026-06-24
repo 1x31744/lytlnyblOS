7779330d233eab005c3c59dc99c8fd92ee4173cd
1x31744 <koopahello@gmail.com>
2026-06-24 04:14:12 +0100

    done ISRS

--- src/kernel/interrupts.asm
+++ src/kernel/interrupts.asm
@@ -10,8 +10,107 @@ idt_load:
     lidt [eax]
     ret
 
-isr0:
-    push dword 0
+isr_common_stub:
+    pusha
+
+    mov ax, ds
+    push eax
+
+    push esp
     call isr_handler
     add esp, 4
+
+    pop eax
+
+    popa
+
+    add esp, 8
     iret
+
+global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
+global isr13
+global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+global isr24
+global isr25
+global isr26
+global isr27
+global isr28
+global isr29
+global isr30
+global isr31
+
+%macro ISR_NOERRCODE 1
+isr%1:
+    push dword 0
+    push dword %1
+    jmp isr_common_stub
+%endmacro
+
+%macro ISR_ERRCODE 1
+isr%1:
+    push dword %1
+    jmp isr_common_stub
+%endmacro
+
+ISR_NOERRCODE 0
+ISR_NOERRCODE 1
+ISR_NOERRCODE 2
+ISR_NOERRCODE 3
+ISR_NOERRCODE 4
+ISR_NOERRCODE 5
+ISR_NOERRCODE 6
+ISR_NOERRCODE 7
+
+ISR_ERRCODE 8
+
+ISR_NOERRCODE 9
+
+ISR_ERRCODE 10
+ISR_ERRCODE 11
+ISR_ERRCODE 12
+ISR_ERRCODE 13
+ISR_ERRCODE 14
+
+ISR_NOERRCODE 15
+ISR_NOERRCODE 16
+
+ISR_ERRCODE 17
+
+ISR_NOERRCODE 18
+ISR_NOERRCODE 19
+ISR_NOERRCODE 20
+ISR_NOERRCODE 21
+ISR_NOERRCODE 22
+ISR_NOERRCODE 23
+ISR_NOERRCODE 24
+ISR_NOERRCODE 25
+ISR_NOERRCODE 26
+ISR_NOERRCODE 27
+ISR_NOERRCODE 28
+ISR_NOERRCODE 29
+
+ISR_ERRCODE 30
+
+ISR_NOERRCODE 31
+
--- src/kernel/interrupts.h
+++ src/kernel/interrupts.h
@@ -34,10 +34,7 @@ typedef struct {
     uint32_t eip;
     uint32_t cs;
     uint32_t eflags;
-    uint32_t user_esp;
-    uint32_t ss; 
-
-} registers_t;
+} __attribute__((packed)) registers_t;
 
 #define IDT_ENTRIES 256
 
@@ -59,5 +56,36 @@ void pic_send_eoi(uint8_t irq);
 extern void idt_load(idtr_t* idtr);
 
 extern void isr0(void);
+extern void isr1(void);
+extern void isr2(void);
+extern void isr3(void);
+extern void isr4(void);
+extern void isr5(void);
+extern void isr6(void);
+extern void isr7(void);
+extern void isr8(void);
+extern void isr9(void);
+extern void isr10(void);
+extern void isr11(void);
+extern void isr12(void);
+extern void isr13(void);
+extern void isr14(void);
+extern void isr15(void);
+extern void isr16(void);
+extern void isr17(void);
+extern void isr18(void);
+extern void isr19(void);
+extern void isr20(void);
+extern void isr21(void);
+extern void isr22(void);
+extern void isr23(void);
+extern void isr24(void);
+extern void isr25(void);
+extern void isr26(void);
+extern void isr27(void);
+extern void isr28(void);
+extern void isr29(void);
+extern void isr30(void);
+extern void isr31(void);
 
 #endif
